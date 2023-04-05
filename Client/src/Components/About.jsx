d9c139cae4fabfa0ab81eb56405ec2ea7156fd3a
unknown <abhisiktj@gmail.com>
2023-04-05 10:56:10 +0530

    rc

--- Client/src/Components/About.jsx
+++ Client/src/Components/About.jsx
@@ -1,6 +1,11 @@
+import {Link} from 'react-router-dom'
 const About=()=>{
     return (
-        <p>About Page</p>
+        <>
+          <p className="text-5xl py-10">😇</p>
+          <p className="text-3xl">Welcome to About Page</p>
+          <Link to="https://www.linkedin.com/in/abhisikt-jha-6285201b8" className='text-2xl font-mono text-blue-900'>LinkedIn</Link>
+        </>
     )
  } 
  export default About;
